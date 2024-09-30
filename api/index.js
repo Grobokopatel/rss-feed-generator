5448e0def857b977a77f4fd1af20602a5edd6ec4
Egor Usov <egotronn@gmail.com>
2024-09-30 23:27:35 +0500

    Я не помню, что я сделал

--- api/index.js
+++ api/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const path = require('node:path');
 const app = express();
-const port = 3000;
+const port = 5000;
 const Parser = require('rss-parser');
 const parser = new Parser();
 
-app.use(express.static('.'));
+app.use(express.static(path.join('static')));
 
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-});
+})*/
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('static', 'index.html'));
+    res.sendFile('index.html');
+});
+
+app.get('/test', (req, res) => {
+    res.send('foobar');
 });
 
 app.put('/user', (req, res) => {
     res.send('Got a PUT request at /user')
 });
 
+
+
+module.exports = app;
 
