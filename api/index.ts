cf1f46bfafed9bbadfbc334a9ba9e65623160ddd
usov <usov@skbkontur.ru>
2025-05-28 02:44:29 +0500

    Подключил typescript и prettier. Рефакторинг

--- api/index.ts
+++ api/index.ts
@@ -1,37 +1,33 @@
 import 'dotenv/config';
 
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-import {HttpsProxyAgent} from "https-proxy-agent";
-import fetch from 'node-fetch';
-import urlNode from "node:url";
-import cons from "@ladjs/consolidate";
-import {Feed} from "feed";
+import { HttpsProxyAgent } from 'https-proxy-agent';
+import fetch, { RequestInit } from 'node-fetch';
+import { resolve } from 'node:url';
+import cons from '@ladjs/consolidate';
+import { Feed, Item } from 'feed';
 import * as cheerio from 'cheerio';
-import {sql} from "@vercel/postgres";
-import path from "node:path";
-import express from "express";
+import { sql } from '@vercel/postgres';
+import { join } from 'node:path';
+import express from 'express';
 import * as util from 'node:util';
-import Hashids from 'hashids'
+import Hashids from 'hashids';
+import { CheerioAPI } from 'cheerio';
+import { FeedSelectors } from '../types/feed-selectors';
+import { Enclosure } from 'feed/lib/typings';
 
-const RUSSIAN_PROXY_URL = process.env.RUSSIAN_PROXY_URL;
+const RUSSIAN_PROXY_URL = process.env.RUSSIAN_PROXY_URL as string;
 
 const app = express();
 
-
 app.engine('handlebars', cons.handlebars);
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '..', 'static')));
-app.set('views', path.join(__dirname, '..', 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, '..', 'static')));
+app.set('views', join(__dirname, '..', 'views'));
 app.set('view engine', 'handlebars');
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
+    console.log(123);
+    res.sendFile(join(__dirname, '..', 'static', 'index.html'));
 });
 
 app.get('/my_feeds/:id', async (req, res) => {
@@ -56,32 +52,30 @@ app.get('/my_feeds/:id', async (req, res) => {
         return;
     }
 
-    let {rows} = queryResult;
+    let { rows } = queryResult;
     if (rows.length === 0) {
         res.status(404).send(`Нет ленты с таким id: ${public_key}`);
         return;
     } else {
-        let {id, url, selectors, last_time_updated, content} = rows[0];
+        let { id, url, selectors, last_time_updated, content } = rows[0];
         try {
             let response = await tryFetchElseFetchWithProxy(url);
             let html = await response.text();
             let $cheerioAPI = await cheerio.load(html);
 
             let post = {
-                link: url
+                link: url,
             };
 
-            let {
-                title,
-                description,
-                imageEnclosure
-            } = await getTitleDescriptionAndImageEnclosure($cheerioAPI, selectors, url);
+            let { title, description, imageEnclosure } =
+                await getTitleDescriptionAndImageEnclosure(
+                    $cheerioAPI,
+                    selectors,
+                    url,
+                );
 
-            post.title = title;
-            post.description = description;
-            post.image = imageEnclosure;
-
-            let currentContent = post.title + ';' + post.description + ';' + imageEnclosure?.url;
+            let currentContent =
+                title + ';' + description + ';' + imageEnclosure?.url;
 
             let lastTimeUpdated = last_time_updated;
             if (content !== currentContent) {
@@ -90,22 +84,25 @@ app.get('/my_feeds/:id', async (req, res) => {
                 await sql`
                     update feed
                     set last_time_updated = ${lastTimeUpdated},
-                        content           = ${currentContent}
+                    content = ${currentContent}
                     where id = ${id};
                 `;
             }
 
-            post.id = url + '#' + +lastTimeUpdated;
-            post.date = lastTimeUpdated;
-
             const feed = new Feed({
-                title: $cheerioAPI('title').html(),
-                description: "Сайт для генерации собственных RSS лент",
+                title: $cheerioAPI('title').html() as string,
+                description: 'Сайт для генерации собственных RSS лент',
                 link: url,
-                language: "ru",
+                language: 'ru',
             });
 
-            feed.addItem(post);
+            feed.addItem({
+                title,
+                description,
+                image: imageEnclosure,
+                id: url + '#' + +lastTimeUpdated,
+                date: lastTimeUpdated,
+            });
             let rssPost = feed.rss2();
             res.contentType('text/xml').send(rssPost);
             return;
@@ -117,17 +114,28 @@ app.get('/my_feeds/:id', async (req, res) => {
     }
 });
 
-async function getTitleDescriptionAndImageEnclosure($cheerioAPI, selectors, url) {
+async function getTitleDescriptionAndImageEnclosure(
+    $cheerioAPI: CheerioAPI,
+    selectors: FeedSelectors,
+    url: string,
+) {
     return {
-        title: $cheerioAPI(selectors.title).prop('innerText'),
-        description: $cheerioAPI(selectors.description).html(),
-        imageEnclosure: await getImageEnclosure(selectors.image, url, $cheerioAPI),
+        title: $cheerioAPI(selectors.title).prop('innerText') as string,
+        description: $cheerioAPI(selectors.description).html() as string,
+        imageEnclosure: (await getImageEnclosure(
+            $cheerioAPI,
+            selectors.image,
+            url,
+        )) as Enclosure,
     };
 }
 
-async function getImageEnclosure(imageSelector, url, $cheerioAPI) {
-    if (!imageSelector)
-        return null;
+async function getImageEnclosure(
+    $cheerioAPI: CheerioAPI,
+    imageSelector: string,
+    url: string,
+) {
+    if (!imageSelector) return null;
 
     let imageElement = $cheerioAPI(imageSelector);
     let imageUrl = imageElement.prop('src');
@@ -135,31 +143,41 @@ async function getImageEnclosure(imageSelector, url, $cheerioAPI) {
         imageUrl = imageElement.find('img[src]').prop('src');
     }
 
-    imageUrl = urlNode.resolve(url, imageUrl);
-    let imageInfo = await tryFetchElseFetchWithProxy(imageUrl, {method: 'HEAD'});
+    imageUrl = resolve(url, imageUrl as string);
+    let imageInfo = await tryFetchElseFetchWithProxy(imageUrl, {
+        method: 'HEAD',
+    });
     let headers = imageInfo.headers;
 
     return {
         url: imageUrl,
         type: headers.get('content-type'),
-        length: parseInt((headers.get('content-length'))),
-    };
+        length: parseInt(headers.get('content-length') as string),
+    } as Enclosure;
 }
 
 app.post('/api/create', async (req, res) => {
     let body = req.body;
-    let selectors = {title: body.title, description: body.description, image: body.image};
-    
+    let selectors: FeedSelectors = {
+        title: body.title,
+        description: body.description,
+        image: body.image,
+    };
+
     let response = await tryFetchElseFetchWithProxy(body.url);
     let html = await response.text();
     let $cheerioAPI = await cheerio.load(html);
     let tab_title = $cheerioAPI('title').text();
-    
+
     const queryResult = await sql`
         select pg_sequence_last_value(pg_get_serial_sequence('feed', 'id'));`;
     let next_id = queryResult.rows[0].pg_sequence_last_value + 1;
-    const public_key = new Hashids(process.env.public_key_salt, 10).encode(next_id);
-    const delete_key = new Hashids(process.env.delete_key_salt, 30).encode(next_id);
+    const public_key = new Hashids(process.env.public_key_salt, 10).encode(
+        next_id,
+    );
+    const delete_key = new Hashids(process.env.delete_key_salt, 30).encode(
+        next_id,
+    );
     let today = new Date();
     try {
         await sql`
@@ -179,32 +197,47 @@ app.post('/api/create', async (req, res) => {
         return;
     }
 
-    res.json({id: public_key, delete_key, url: body.url, tab_title, creation_date: today});
+    res.json({
+        id: public_key,
+        delete_key,
+        url: body.url,
+        tab_title,
+        creation_date: today,
+    });
 });
 
 app.get('/link-to-feed', async (req, res) => {
     let public_key = req.query.id;
-    res.render('link-to-feed',{
-        url: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/my_feeds/${public_key}`
-    })
-})
+    res.render('link-to-feed', {
+        id: public_key,
+    });
+});
 
 app.post('/feed-preview', async (req, res) => {
     let body = req.body;
     let response = await tryFetchElseFetchWithProxy(body.url);
     let html = await response.text();
     let $cheerioAPI = await cheerio.load(html);
-    let selectors = {title: body.title, description: body.description, image: body.image};
-    let {
+    let selectors = {
+        title: body.title,
+        description: body.description,
+        image: body.image,
+    };
+    let { title, description, imageEnclosure } =
+        await getTitleDescriptionAndImageEnclosure(
+            $cheerioAPI,
+            selectors,
+            body.url,
+        );
+    res.render('feed-preview', {
         title,
         description,
-        imageEnclosure
-    } = await getTitleDescriptionAndImageEnclosure($cheerioAPI, selectors, body.url);
-    res.render('feed-preview', {title, description, image: imageEnclosure?.url});
+        image: imageEnclosure?.url,
+    });
 });
 
 app.post('/api/delete', async (req, res) => {
-    let {delete_key} = req.body;
+    let { delete_key } = req.body;
     let result;
     try {
         result = await sql`
@@ -222,14 +255,19 @@ app.post('/api/delete', async (req, res) => {
     } else {
         res.sendStatus(200);
     }
-})
+});
 
-async function tryFetchElseFetchWithProxy(url, options = {}) {
+async function tryFetchElseFetchWithProxy(
+    url: string,
+    options: RequestInit = {},
+) {
     try {
         let response = await fetch(url, options);
         if (!response.ok) {
-            throw new Error(`Got response code outside of 200-299: ${response.status} ${response.statusText}.\n` +
-                `Headers: ${util.inspect(response.headers)}`);
+            throw new Error(
+                `Got response code outside of 200-299: ${response.status} ${response.statusText}.\n` +
+                    `Headers: ${util.inspect(response.headers)}`,
+            );
         }
 
         return response;
@@ -240,13 +278,18 @@ async function tryFetchElseFetchWithProxy(url, options = {}) {
         let response = await fetch(url, options);
 
         if (!response.ok) {
-            throw new Error(`Got response code outside of 200-299: ${response.status} ${response.statusText}.\n` +
-                `Headers: ${util.inspect(response.headers)}`);
+            throw new Error(
+                `Got response code outside of 200-299: ${response.status} ${response.statusText}.\n` +
+                    `Headers: ${util.inspect(response.headers)}`,
+            );
         }
 
         let headers = response.headers;
-        headers.set('content-length', headers.get('zr-content-length'));
-        headers.set('content-type', headers.get('zr-content-type'));
+        headers.set(
+            'content-length',
+            headers.get('zr-content-length') as string,
+        );
+        headers.set('content-type', headers.get('zr-content-type') as string);
 
         return response;
     }
---
+++ types/feed-selectors.ts
@@ -0,0 +1,5 @@
+export type FeedSelectors = {
+    title: string;
+    description: string;
+    image: string;
+};
